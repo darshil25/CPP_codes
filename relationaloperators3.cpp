b3e45b54e5ef0c9a2cf5b8f5e6c3605e53d8f41f
Darshil Thummar <thummardarshil9687@gmail.com>
2023-04-25 22:28:53 +0530

    Add files via upload

---
+++ relationaloperators3.cpp
@@ -0,0 +1,12 @@
+/*BITWISE OPERATORS
+-> & (and)
+-> | (or)
+-> ^ (XOR) = jo bey operands ni value same hase to 0 return thase ne alag alag hase to 1.
+-> ~ (ones) = 0 input kariye to 1 return ape ne 1 kariye to 0
+-> << (left shift) = number na bits ne left ma shift kare
+    ex.4<<1 (0100) = 1000 [<<x ahi x key ke ketla place nu shift karvanu 6 ]
+    equation - a<<n = a x 2^n
+-> >> (right shift) = number na bits ne right ma shift kare
+    ex.4>>1 (0100) = 0010 [>>x ahi x key ke ketla place nu shift karvanu 6 ]
+    equation - a>>n = a / 2^n
+    */
